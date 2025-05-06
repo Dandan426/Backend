4e01f4b0bf38fec500d9921d38dbfd49114776b7
Dandan426 <danielanunesmorai23@gmail.com>
2025-05-06 16:44:20 -0300

    Criando Middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+
+const jwt = require("jsonwebtoken");
+
+// Middleware -> Autenticação 
+export function authenticate(req, res, next) {
+    // capturando token da requisição
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader("Bearer")) {
+        return res.status(401).json({
+            msg: "Token não fornecido"
+        });
+    }
+}
